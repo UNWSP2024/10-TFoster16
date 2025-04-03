969c88877ad34b6e3851c2aded5e12a5d7fe78b8
TFoster16 <northwesterntim@gmail.com>
2025-04-03 09:58:04 -0500

    Employee.py

--- Employee.py
+++ Employee.py
@@ -1,10 +1,58 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+#Timothy Foster, 3/26/25, Employee Class Program
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+#Define the class.
+class Employee:
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+    #Initialize the class.
+    def __init__(self, name, ID_number, department, job_title):
+        self.__name = name
+        self.__ID_number = ID_number
+        self.__department = department
+        self.__job_title = job_title
+
+    #Define the sets.
+    def set_name(self, name):
+        self.__name = name
+
+    def set_ID_number(self, ID_number):
+        self.__ID_number = ID_number
+
+    def set_department(self, department):
+        self.__department = department
+
+    def set_job_title(self, job_title):
+        self.__job_title = job_title
+
+    #Define the gets.
+    def get_name(self):
+        return self.__name
+
+    def get_ID_number(self):
+        return self.__ID_number
+
+    def get_department(self):
+        return self.__department
+
+    def get_job_title(self):
+        return self.__job_title
+
+#Define the function.
+def create_and_print_employee_data():
+
+    #Make new objects with the Employee class.
+    employee1 = Employee("Susan Meyers", 47899, "Accounting", "Vice President")
+    employee2 = Employee("Mark Jones", 39119, "IT", "Programmer")
+    employee3 = Employee("Joy Rogers", 81774, "Manufacturing", "Engineer")
+
+    #Call data from the each class and assign it to the variable.
+    employee1_info = "Employee 1: " + f"{employee1.get_name()}, " + f"{employee1.get_ID_number()}, " + f"{employee1.get_department()}, " + f"{employee1.get_job_title()}"
+    employee2_info = "Employee 2: " + f"{employee2.get_name()}, " + f"{employee2.get_ID_number()}, " + f"{employee2.get_department()}, " + f"{employee2.get_job_title()}"
+    employee3_info = "Employee 3: " + f"{employee3.get_name()}, " + f"{employee3.get_ID_number()}, " + f"{employee3.get_department()}, " + f"{employee3.get_job_title()}"
+
+    #Print the results.
+    print(employee1_info)
+    print(employee2_info)
+    print(employee3_info)
+
+if __name__ == "__main__":
+    create_and_print_employee_data()
